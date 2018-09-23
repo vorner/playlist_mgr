a50bed8b88299b37b466627a990662f685e5a4c0
Michal 'vorner' Vaner <vorner@vorner.cz>
2018-09-23 17:46:04 +0200

    The player implementation

---
+++ src/main.rs
@@ -0,0 +1,173 @@
+use std::collections::HashSet;
+use std::io::{self, BufRead, BufReader};
+use std::ffi::{OsStr, OsString};
+use std::os::unix::ffi::OsStringExt;
+use std::path::PathBuf;
+use std::process;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use corona::io::BlockingWrapper;
+use corona::prelude::*;
+use failure::Error;
+use futures::unsync::oneshot;
+use log::{debug, error, info, trace, warn};
+use tokio::net::unix::UnixListener;
+use tokio::io::AsyncRead;
+
+macro_rules! catch {
+    ($( $b: tt )*) => {
+        (|| -> Result<_, Error> { Ok({ $( $b )* } ) })()
+    };
+}
+
+mod player;
+
+use self::player::{Cmd, Mode};
+
+static CONN_NUM: AtomicUsize = AtomicUsize::new(0);
+const FORBIDDEN_EXTS: &[&str] = &[
+    "htm",
+    "html",
+    "jpg",
+    "jpeg",
+    "ini",
+    "bmp",
+    "db",
+    "doc",
+    "dtt",
+    "gif",
+    "listing",
+    "m3u",
+    "nfo",
+    "out",
+    "pls",
+    "txt",
+    "toc",
+    "zip",
+];
+
+fn handle_cmd(cmd: &[u8], lines: impl Iterator<Item = Result<Vec<u8>, io::Error>>)
+    -> Result<bool, Error>
+{
+    let mut split = cmd.split(|c| *c == b' ')
+        .filter(|word| !word.is_empty());
+    if let Some(cmd) = split.next() {
+        match cmd {
+            b"mode" => {
+                let mode = match split.next() {
+                    Some(b"random") => Mode::Random,
+                    Some(b"sequence") => Mode::Sequence,
+                    Some(b"circular") => Mode::Circular,
+                    Some(unknown) => {
+                        error!("Unknown mode {}", String::from_utf8_lossy(unknown));
+                        return Ok(true);
+                    }
+                    None => {
+                        error!("Missing mode");
+                        return Ok(true);
+                    }
+                };
+                player::send(Cmd::Mode(mode));
+            },
+            b"load" => {
+                let flags = split.collect::<HashSet<_>>();
+                let append = flags.contains(b"append" as &[_]);
+                // Go until you find the first empty line
+                let mut songs = Vec::new();
+                for line in lines {
+                    let line = line?;
+                    if line.is_empty() {
+                        // End of block
+                        break;
+                    }
+
+                    let path = PathBuf::from(OsString::from_vec(line));
+
+                    if !path.is_file() {
+                        warn!("Non-file {} in list of songs", path.to_string_lossy());
+                        continue;
+                    }
+                    let forbidden = path.extension()
+                        .and_then(OsStr::to_str)
+                        .map(|ext| {
+                            FORBIDDEN_EXTS
+                                .iter()
+                                .find(|forbidden| forbidden.eq_ignore_ascii_case(ext))
+                                .is_some()
+                        }).unwrap_or(false);
+                    if forbidden {
+                        trace!("Skipping forbidden file {}", path.to_string_lossy());
+                        continue;
+                    }
+
+                    songs.push(path);
+                }
+                player::send(Cmd::Load { append, songs });
+            }
+            b"quit" => return Ok(false),
+            b"terminate" => {
+                player::send(Cmd::Stop);
+                let (sender, receiver) = oneshot::channel();
+                player::send(Cmd::Confirm(sender));
+                let _ = receiver.coro_wait();
+                process::exit(0);
+            }
+            b"play" => player::send(Cmd::Play),
+            b"next" => player::send(Cmd::Next),
+            b"prev" => player::send(Cmd::Prev),
+            b"stop" => player::send(Cmd::Stop),
+            _ => error!("Unknown command {}", String::from_utf8_lossy(cmd)),
+        }
+    } // Else → empty command, ignore
+    Ok(true)
+}
+
+fn handle_conn(conn: impl AsyncRead) {
+    let num = CONN_NUM.fetch_add(1, Ordering::Relaxed);
+    info!("Accepted a control connection #{}", num);
+    let mut lines = BufReader::new(BlockingWrapper::new(conn)).split(b'\n');
+    let result = catch! {
+        loop {
+            let line = lines.next();
+            match line {
+                None => {
+                    info!("Connection closed #{}", num);
+                    break;
+                }
+                Some(cmd) => if !handle_cmd(&cmd?, &mut lines)? {
+                    info!("Closing connection #{}", num);
+                    break;
+                },
+            }
+        }
+    };
+    if let Err(e) = result {
+        error!("Error on connection #{}: {}", num, e);
+    }
+}
+
+fn main() {
+    env_logger::init();
+    let result = Coroutine::new()
+        .stack_size(65_536)
+        .run(|| -> Result<(), Error> {
+            // TODO: Configure
+            // TODO: Signals
+            let listener = UnixListener::bind("/home/vorner/.clue_play_socket")?;
+            debug!("Created listening socket");
+            for socket in listener.incoming().iter_result() {
+                match socket {
+                    Ok(socket) => {
+                        corona::spawn(move || handle_conn(socket));
+                    }
+                    Err(err) => error!("Failed to accept connection: {}", err),
+                }
+            }
+            unreachable!()
+        }).unwrap();
+
+    if let Err(e) = result {
+        error!("Top level error: {}", e);
+        process::exit(1);
+    }
+}
---
+++ src/player.rs
@@ -0,0 +1,295 @@
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::io::{Error as IoError, Write};
+use std::os::unix::io::AsRawFd;
+use std::os::unix::process::CommandExt as UnixCommandExt;
+use std::os::unix::net::UnixStream as StdUnixStream;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
+use std::time::{Duration, Instant};
+
+use corona::io::BlockingWrapper;
+use corona::prelude::*;
+use failure::Error;
+use futures::unsync::oneshot::Sender;
+use futures::unsync::mpsc::{self, UnboundedSender as QueueSender};
+use id3::Tag;
+use log::{debug, error, info};
+use nix::unistd;
+use rand::Rng;
+use tokio::reactor::Handle;
+use tokio::net::unix::UnixStream;
+use tokio_process::CommandExt;
+
+#[derive(Debug, Eq, PartialEq)]
+pub(crate) enum Mode {
+    Random,
+    Sequence,
+    Circular,
+}
+
+#[derive(Debug)]
+pub(crate) enum Cmd {
+    Play,
+    Stop,
+    Next,
+    Prev,
+    Load {
+        songs: Vec<PathBuf>,
+        append: bool,
+    },
+    Mode(Mode),
+    Confirm(Sender<()>),
+    Done,
+}
+
+struct Player {
+    mode: Mode,
+    songs: Vec<PathBuf>,
+    history: VecDeque<PathBuf>,
+    playlist: Vec<PathBuf>,
+    current: Option<PathBuf>,
+    should_play: bool,
+    position: usize,
+    control_pipe: Option<BlockingWrapper<UnixStream>>,
+    last_start: Option<Instant>,
+}
+
+impl Player {
+    fn new() -> Self {
+        Player {
+            mode: Mode::Random,
+            songs: Vec::new(),
+            history: VecDeque::new(),
+            playlist: Vec::new(),
+            current: None,
+            should_play: false,
+            position: 0,
+            control_pipe: None,
+            last_start: None,
+        }
+    }
+
+    fn done(&mut self) {
+        if let Some(current) = self.current.take() {
+            self.history.push_back(current);
+            while self.history.len() > 100 {
+                self.history.pop_front();
+            }
+        }
+
+        self.control_pipe = None;
+        self.last_start = None;
+
+        if self.should_play {
+            self.start();
+        }
+    }
+
+    fn choose_song(&mut self) -> Option<PathBuf> {
+        if let Some(song) = self.playlist.pop() {
+            return Some(song);
+        }
+
+        if self.songs.is_empty() {
+            return None;
+        }
+
+        match self.mode {
+            Mode::Random => self.position = rand::thread_rng().gen_range(0, self.songs.len()),
+            Mode::Sequence if self.position > self.songs.len() => self.position = 0,
+            Mode::Circular if self.position >= self.songs.len() => self.position = 0,
+            _ => (),
+        }
+
+        let next = self.songs.get(self.position).cloned();
+
+        self.position += 1;
+
+        next
+    }
+
+
+    fn start(&mut self) {
+        if let Some(song) = self.choose_song() {
+            assert!(self.control_pipe.is_none());
+            assert!(self.current.is_none());
+
+            let child = catch! {
+                debug!("Starting mpv with {}", song.to_string_lossy());
+
+                let (sender, receiver) = StdUnixStream::pair()?;
+
+                let receiver_fd = receiver.as_raw_fd();
+
+                let child = Command::new("/usr/bin/mpv")
+                    .args(&["-really-quiet", "-vo", "null", "--input-file=fd://4"])
+                    .arg(&song)
+                    .stdin(Stdio::null())
+                    .stdout(Stdio::null())
+                    .stderr(Stdio::null())
+                    .before_exec(move || {
+                        unistd::dup2(receiver_fd, 4)
+                            .map(|_| ())
+                            .map_err(|_| IoError::last_os_error())
+                    }).spawn_async()?;
+
+                let sender = UnixStream::from_std(sender, &Handle::default())?;
+                (child, sender)
+            };
+
+            let info = Tag::read_from_path(&song)
+                .map(|tag| {
+                    let album = tag.album_artist().unwrap_or("???");
+                    let title = tag.title().unwrap_or("???");
+                    format!("{} ({})", title, album)
+                }).unwrap_or_else(|_| "???".to_owned());
+
+            println!("• {}\n  {}", info, song.to_string_lossy());
+
+            match child {
+                Err(e) => {
+                    error!("Failed to start mpv: {}", e);
+                    self.should_play = false;
+                }
+                Ok((child, control)) => {
+                    self.control_pipe = Some(BlockingWrapper::new(control));
+                    self.current = Some(song);
+                    self.last_start = Some(Instant::now());
+
+                    corona::spawn(move || {
+                        match child.coro_wait() {
+                            Err(e) => error!("Error waiting for mpv: {}", e),
+                            Ok(status) => if status.success() {
+                                debug!("Terminated successfully");
+                            } else {
+                                error!("Mpv: {}", status);
+                            }
+                        }
+
+                        send(Cmd::Done);
+                    });
+                }
+            }
+        } else {
+            info!("Nothing to play");
+            self.should_play = false;
+        }
+    }
+
+    fn send_mpv(&mut self, key: &[u8]) {
+        if let Some(control) = self.control_pipe.as_mut() {
+            debug!("Sending command {}", String::from_utf8_lossy(key));
+            // It might fail if the other end terminates, right?
+            let _ = control.write_all(key);
+        } else {
+            debug!("Nowhere to send command {}", String::from_utf8_lossy(key));
+        }
+    }
+
+    fn pause(&mut self) {
+        self.send_mpv(b"pause\n");
+    }
+
+    fn play_pause(&mut self) {
+        self.should_play = true;
+        if self.control_pipe.is_some() {
+            self.pause();
+        } else {
+            self.start();
+        }
+    }
+
+    fn next(&mut self) {
+        self.should_play = true;
+
+        if self.control_pipe.is_some() {
+            self.stop_song();
+        } else {
+            self.start();
+        }
+    }
+
+    fn prev(&mut self) {
+        if let Some(current) = self.current.take() {
+            self.playlist.push(current);
+        }
+
+        // If the current song (we just moved above) played for long enough, restart it. If not,
+        // place the previous song before it.
+        let restart = self.last_start.take()
+            .map(|last| Instant::now() - last > Duration::from_secs(2))
+            .unwrap_or(false);
+
+        if !restart {
+            if let Some(prev) = self.history.pop_back() {
+                self.playlist.push(prev);
+            }
+        }
+
+        // Going forward instead of back if there's no back would be stupid
+        if !self.playlist.is_empty() {
+            // We just pick the next song from the playlist ‒ the one we just placed there.
+            self.next();
+        }
+    }
+
+    fn stop(&mut self) {
+        self.should_play = false;
+        self.stop_song();
+    }
+
+    fn stop_song(&mut self) {
+        self.send_mpv(b"quit\n");
+    }
+
+    fn cmd(&mut self, cmd: Cmd) {
+        use self::Cmd::*;
+
+        debug!("Executing command {:#?}", cmd);
+
+        match cmd {
+            Play => self.play_pause(),
+            Stop => self.stop(),
+            Next => self.next(),
+            Prev => self.prev(),
+            Load { songs, append } => {
+                if append {
+                    self.songs.extend(songs);
+                } else {
+                    self.songs = songs;
+                    self.position = 0;
+                }
+            }
+            Mode(mode) => self.mode = mode,
+            Confirm(sender) => {
+                let _ = sender.send(());
+            }
+            Done => self.done(),
+        }
+    }
+}
+
+fn start_player() -> QueueSender<Cmd> {
+    let (sender, receiver) = mpsc::unbounded();
+
+    corona::spawn(move || {
+        let mut player = Player::new();
+
+        for cmd in receiver.iter_ok() {
+            player.cmd(cmd);
+        }
+        unreachable!();
+    });
+
+    sender
+}
+
+thread_local! {
+    // Thread local for a single-threaded application ‒ but rust otherwise insists on mutexes
+    static QUEUE: RefCell<QueueSender<Cmd>> = RefCell::new(start_player());
+}
+
+pub(crate) fn send(cmd: Cmd) {
+    let _ = QUEUE.with(|q| q.borrow_mut().unbounded_send(cmd));
+}
